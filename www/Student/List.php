6258791db116b6948e63967e6da4e6ff08bba605
tranquilblade <leojames@leoMac.local>
2013-11-06 21:02:13 -0500

    Initial set

---
+++ www/Student/List.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="81%" border="0" align="center">
+  <tr>
+    <td colspan="5" bgcolor="#FBD8C9" style="text-align: center"> Test History</td>
+  </tr>
+  <tr>
+    <td width="8%" bgcolor="#A3D4FA" style="text-align: center">ID</td>
+    <td width="42%" bgcolor="#A3D4FA" style="text-align: center">Title</td>
+    <td width="17%" bgcolor="#A3D4FA" style="text-align: center">Date</td>
+    <td width="17%" bgcolor="#A3D4FA" style="text-align: center">Score</td>
+    <td width="16%" bgcolor="#A3D4FA" style="text-align: center">Detail</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Being A Star Athlete</td>
+    <td style="text-align: center">9/11/2013</td>
+    <td style="text-align: center">83</td>
+    <td style="text-align: center"><input type="button" name="button4" id="button4" value="&gt;&gt;" onClick="javascript:window.location='Result.php'" ></td>
+  </tr>
+  <tr>
+    <td bgcolor="#E7E7E7">2</td>
+    <td bgcolor="#E7E7E7">Disability</td>
+    <td bgcolor="#E7E7E7" style="text-align: center">9/12/2013</td>
+    <td bgcolor="#E7E7E7" style="text-align: center">49</td>
+    <td bgcolor="#E7E7E7" style="text-align: center"><input type="button" name="button5" id="button5" value="&gt;&gt;" onClick="javascript:window.location='Result.php'" ></td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Language Problem</td>
+    <td style="text-align: center">9/13/2013</td>
+    <td style="text-align: center">66</td>
+    <td style="text-align: center"><input type="button" name="button6" id="button6" value="&gt;&gt;" onClick="javascript:window.location='Result.php'" ></td>
+  </tr>
+  <tr>
+    <td bgcolor="#E7E7E7">4</td>
+    <td bgcolor="#E7E7E7">My Daily Life</td>
+    <td bgcolor="#E7E7E7" style="text-align: center">9/15/2013</td>
+    <td bgcolor="#E7E7E7" style="text-align: center">43</td>
+    <td bgcolor="#E7E7E7" style="text-align: center"><input type="button" name="button7" id="button7" value="&gt;&gt;" onClick="javascript:window.location='Result.php'" ></td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td bgcolor="#FFFFFF">Our Backyard</td>
+    <td style="text-align: center">10/11/2013</td>
+    <td style="text-align: center">26</td>
+    <td style="text-align: center"><input type="button" name="button8" id="button8" value="&gt;&gt;" onClick="javascript:window.location='Result.php'" ></td>
+  </tr>
+  <tr>
+    <td bgcolor="#E7E7E7">6</td>
+    <td bgcolor="#E7E7E7">Outdoor Sports</td>
+    <td bgcolor="#E7E7E7" style="text-align: center">10/12/2013</td>
+    <td bgcolor="#E7E7E7" style="text-align: center">Waiting</td>
+    <td bgcolor="#E7E7E7" style="text-align: center"><input type="button" name="button9" id="button9" value="&gt;&gt;" onClick="javascript:window.location='Result.php'" ></td>
+  </tr>
+  <tr>
+    <td>7</td>
+    <td>Keep Practice</td>
+    <td style="text-align: center">10/13/2013</td>
+    <td style="text-align: center">Waiting</td>
+    <td style="text-align: center"><input type="button" name="button10" id="button10" value="&gt;&gt;" onClick="javascript:window.location='Result.php'" ></td>
+  </tr>
+  <tr>
+    <td bgcolor="#FBD8C9">&nbsp;</td>
+    <td bgcolor="#FBD8C9" style="text-align: center"><input type="button" name="button" id="button" value="Start New.." onClick="javascript:window.location='Read.php'" ></td>
+    <td bgcolor="#FBD8C9" style="text-align: center"><input type="button" name="button2" id="button2" value="Help" onClick="javascript:window.location='Guidance.php'" ></td>
+    <td bgcolor="#FBD8C9" style="text-align: center"><input type="button" name="button3" id="button3" value="Log out" onClick="javascript:window.location='../index.php'" ></td>
+    <td bgcolor="#FBD8C9">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ www/Student/Result.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="72%" border="0" align="center">
+  <tr>
+    <td colspan="6" bgcolor="#FBD8C9" style="text-align: center">Read &amp; Record</td>
+  </tr>
+  <tr>
+    <td colspan="6" bgcolor="#A3D4FA" style="text-align: center">Reading <span style="text-align: center"></span>Title</td>
+  </tr>
+  <tr>
+    <td height="238" colspan="6" bgcolor="#E7E7E7" style="text-align: center">Reading Content/ Mark Results</td>
+  </tr>
+  <tr>
+    <td width="23%" height="31" bgcolor="#A3D4FA" style="text-align: center">Correct</td>
+    <td width="11%" bgcolor="#A3D4FA" style="text-align: center">87</td>
+    <td width="21%" height="31" bgcolor="#A3D4FA" style="text-align: center">Incorrect</td>
+    <td width="11%" height="31" bgcolor="#A3D4FA" style="text-align: center">12</td>
+    <td width="20%" height="31" bgcolor="#A3D4FA" style="text-align: center">Score</td>
+    <td width="14%" bgcolor="#A3D4FA" style="text-align: center"> 83</td>
+  </tr>
+  <tr>
+    <td colspan="2" bgcolor="#FBD8C9" style="text-align: center">Recorded Time: 01:32</td>
+    <td colspan="2" bgcolor="#FBD8C9" style="text-align: center"><input type="button" name="button" id="button" value="Play"></td>
+    <td colspan="2" bgcolor="#FBD8C9" style="text-align: center"><input type="button" name="button3" id="button3" value="Back" onClick="javascript:window.location='List.php'"></td>
+  </tr>
+</table>
+</body>
+</html>
